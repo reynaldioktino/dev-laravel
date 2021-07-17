bf82e394143b612d3d979b6ef989122e48e023da
reynaldioktino <reynaldiolp@gmail.com>
2021-07-17 15:47:38 +0700

    routing

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dev Laravel | Blog</title>
+</head>
+
+<body>
+    <h1>Halaman Blog</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'name'  =>  'Reynaldi'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
 });
